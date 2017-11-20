937a5f76bd312841a4649923a247f6359dbfba79
karthikmunsu <karthikmunsu@gmail.com>
2017-11-20 18:25:21 +0530

    pm2 added error fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,49 +7,48 @@ import { AppService } from './app.service';
 providers: [AppService]
 })
 export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked{
-  title = 'app';
+  title = 'Demoapp';
 constructor( private appService: AppService,){}
 public images = new Array();
 
 public getData() {
-let t1 = 'test';
-this.appService.fetchData(t1).subscribe(
-      (res) => {
-        //console.log(res,res.length);
-//this.images = res;
-for(let i =0;i<res.length;i++){
-if(this.checkImage(res[i])){
-this.images.push(res[i]);
-}
-}
+  let t1 = 'test';
+  this.appService.fetchData(t1).subscribe(
+        (res) => {
+    for(let i =0;i<res.length;i++){
+      if(this.checkImage(res[i])){
+        this.images.push(res[i]);
+      }
+    }
       }, (err) => {
         console.log(err);
       });
-
 }
 
 public refresh(){
-this.images=null;
-this.getData();
+  this.images=null;
+  this.getData();
 }
 
 public checkImage(data: any) {
-let tmp = data.split('.');
-console.log('check ',tmp);
-if(tmp[tmp.length-1] === 'jpg' || tmp[tmp.length-1] === 'jpeg') {
-return true;
-}else {
-return false;
+  let tmp = data.split('.');
+    console.log('check ',tmp);
+  if(tmp[tmp.length-1] === 'jpg' || tmp[tmp.length-1] === 'jpeg') {
+    return true;
+  }else {
+  return false;
+}
 }
 
-public urlAppend(data: any) {
+public urlAppend(data: any):any {
   return 'http://localhost:3000/imgfile/?image='+data;
 }
- public ngAfterViewInit() {
-   this.getData();
-    }
 
-  public ngOnInit() {}
+public ngAfterViewInit() {
+   this.getData();
+}
+
+public ngOnInit() {}
 public ngAfterViewChecked(): void {}
 
 }
